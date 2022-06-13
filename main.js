c46a59074b523f9295129cce890e0488b4e366b8
Samet Solaklar <ag179866@gmail.com>
2022-06-13 16:33:33 +0300

    Upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typingEffect = new Typed(".multiText",{
+			strings: ["programmer!", "junior dev!", "student!", "web dev!"],
+			loop : true,
+			typeSpeed: 100,
+			backSpeed: 80,
+			backDelay: 1500
+		});
+
+document.querySelector("#gt").onclick=function(){
+	window.open("https://www.github.com/sametsolaklar", "_blank"),
+	window.location.href = ""
+	
+}
+
